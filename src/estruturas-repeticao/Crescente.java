a72ae33ac1230f3c04864312a9ba6151bd474136
Hadryan <hadryan2001@gmail.com>
2022-03-04 09:28:44 -0300

    Nova Classe

--- src/estruturas-repeticao/Crescente.java
+++ src/estruturas-repeticao/Crescente.java
@@ -1,7 +1,30 @@
-package estruturas-repeticao;
+import java.util.Scanner;
 
 public class Crescente {
     public static void main(String[] args) {
+        int x, y;
+        Scanner leia = new Scanner(System.in);
         
+        System.out.print("Digite o valor de X: ");
+        x = leia.nextInt();
+        System.out.print("Digite o valor de Y: ");
+        y = leia.nextInt();
+
+        while(x != y) {
+            
+            if(x > y) {
+                System.out.println("Decrescente");
+            }else {
+                System.out.println("Crescente");
+            }
+
+            System.out.print("Digite o valor de X: ");
+            x = leia.nextInt();
+            System.out.print("Digite o valor de Y: ");
+            y = leia.nextInt();
+
+        }
+
+        leia.close();
     }
 }
