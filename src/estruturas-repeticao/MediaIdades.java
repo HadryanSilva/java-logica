0fcfd9ab3b8ee05c036bc4ef227c1018b966fbd6
Hadryan <hadryan2001@gmail.com>
2022-03-04 10:16:53 -0300

    Nova Classe

--- src/estruturas-repeticao/MediaIdades.java
+++ src/estruturas-repeticao/MediaIdades.java
@@ -1,2 +1,32 @@
+import java.util.Scanner;
+
 public class MediaIdades {
+    public static void main(String[] args) {
+        int idade = 0;
+        int idades = 0;
+        int media = 0;
+        int contador = 1;
+        Scanner leia = new Scanner(System.in);
+
+        System.out.println("Digite a Idade: ");
+        idade = leia.nextInt();
+
+        if(idade < 0) {
+            System.out.println("IMPOSSIVEL CALCULAR");
+            idades = 0;
+        }
+
+        while(idade >= 0) {
+            System.out.println("Digite a Idade: ");
+            idade = leia.nextInt();
+
+            if(idade > 0) {
+                idades += idade;
+                contador++;
+
+            }
+        }
+            media = idades / contador;
+            System.out.println("Media: " + media);
+    }
 }
