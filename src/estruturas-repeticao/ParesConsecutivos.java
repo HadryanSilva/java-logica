ab5660a790201c887ad9411c2b2c36365beed51a
HadryanSilva <hadryan2001@gmail.com>
2022-03-13 14:02:44 -0300

    Estruturas Repetitivas

---
+++ src/estruturas-repeticao/ParesConsecutivos.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class ParesConsecutivos {
+
+    public static void main(String[] args) {
+        Scanner leia = new Scanner(System.in);
+        int x = 1;
+        int soma = 0;
+
+        while (x != 0) {
+            System.out.print("Digite o valor de X: ");
+            x = leia.nextInt();
+
+            if(x != 0 && x % 2 == 0) {
+                for(int i = 0; i < 5; i++) {
+                    soma += x + 2;
+                }
+                System.out.println("SOMA: " + soma);
+            } else if(x != 0) {
+                soma = x += 1;
+                for(int i = 0; i < 4; i++) {
+                    soma += (x += 2);
+                }
+                System.out.println("SOMA: " + soma);
+            }
+        }
+
+
+
+    }
+}
