da787569f874b7c040e6d6e93e2edee28c4d86fe
HadryanSilva <hadryan2001@gmail.com>
2022-03-13 16:29:32 -0300

    Estruturas Repetitivas

---
+++ src/br/com/logica/repeticao/DentroOuFora.java
@@ -0,0 +1,37 @@
+package br.com.logica.repeticao;
+
+import java.util.Scanner;
+
+public class DentroOuFora {
+
+    /*
+        Leia um valor inteiro N. Este valor será a quantidade de valores inteiros X que serão lidos em seguida.
+        Mostre quantos destes valores X estão dentro do intervalo [10,20] e quantos estão fora do intervalo,
+        conforme exemplo
+     */
+
+    public static void main(String[] args) {
+
+        Scanner leia = new Scanner(System.in);
+        int x, y  = 0;
+        int dentro = 0;
+        int fora = 0;
+
+        System.out.print("Quantos numero você quer digitar? ");
+        x = leia.nextInt();
+
+        for (int i = 0; i < x; i++) {
+            System.out.print("Digite um Numero: ");
+            y = leia.nextInt();
+
+            if (y >= 10 && y <= 20) {
+               dentro +=  1;
+            } else {
+                fora += 1;
+            }
+        }
+
+        System.out.println(dentro + " Numeros Dentro do Conjunto\n" + fora + " Numeros Fora do Conjunto");
+    }
+
+}
