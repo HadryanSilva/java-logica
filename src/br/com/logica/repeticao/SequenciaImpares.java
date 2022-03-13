412ab9fec0b48fcfe75472c5199791a8a2a36b16
HadryanSilva <hadryan2001@gmail.com>
2022-03-13 16:00:24 -0300

    Estruturas Repetitivas

---
+++ src/br/com/logica/repeticao/SequenciaImpares.java
@@ -0,0 +1,31 @@
+package br.com.logica.repeticao;
+
+import java.util.Scanner;
+
+public class SequenciaImpares {
+
+    /*
+        Leia um valor inteiro X. Em seguida mostre os ímpares de 1 até X, um valor por linha, inclusive o X,
+        se for o caso.
+     */
+
+    public static void main(String[] args) {
+
+        Scanner leia = new Scanner(System.in);
+        int x;
+
+        System.out.print("Digite o valor de X: ");
+        x = leia.nextInt();
+
+        if (x % 2 == 0) {
+            for (int i = 1; i < x;i += 2) {
+                System.out.println(i);
+            }
+        } else {
+            for (int i = 1; i <= x;i += 2) {
+                System.out.println(i);
+            }
+        }
+    }
+
+}
